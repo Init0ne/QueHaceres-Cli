061426b6b564697dcae0d6bd5a32fcb1f2eae3fd
Mathias Falvo <falvomathias@gmail.com>
2025-01-25 15:23:06 -0300

    * Correcciones de codigo!

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace QueHaceres_Cli
         {
             FileHelper.EnsureFileExists();
 
-            Console.WriteLine("Bienvenido a QueHaceres-Cli. Escribe 'help' para ver los comandos disponibles.");
+            Console.WriteLine("Bienvenido a QueHaceres-Cli. Escribe 'ayuda' para ver los comandos disponibles.");
             string input;
             do
             {
@@ -25,10 +25,10 @@ namespace QueHaceres_Cli
 
                 switch (command)
                 {
-                    case "add":
+                    case "agregar":
                         if (string.IsNullOrWhiteSpace(arguments))
                         {
-                            Console.WriteLine("Uso: add \"descripcion de la tarea\"");
+                            Console.WriteLine("Uso: agregar \"descripcion de la tarea\"");
                         }
                         else
                         {
@@ -36,11 +36,11 @@ namespace QueHaceres_Cli
                         }
                         break;
 
-                    case "update":
+                    case "actualizar":
                         string[] updateParts = arguments.Split(' ', 2);
                         if (updateParts.Length < 2 || !int.TryParse(updateParts[0], out int updateId))
                         {
-                            Console.WriteLine("Uso: update [ID] \"nueva descripcion\"");
+                            Console.WriteLine("Uso: actualizar [ID] \"nueva descripcion\"");
                         }
                         else
                         {
@@ -48,10 +48,10 @@ namespace QueHaceres_Cli
                         }
                         break;
 
-                    case "delete":
+                    case "eliminar":
                         if (!int.TryParse(arguments, out int deleteId))
                         {
-                            Console.WriteLine("Uso: delete [ID]");
+                            Console.WriteLine("Uso: eliminar [ID]");
                         }
                         else
                         {
@@ -59,11 +59,11 @@ namespace QueHaceres_Cli
                         }
                         break;
 
-                    case "change-status":
+                    case "cambiar-estado":
                         string[] statusParts = arguments.Split(' ', 2);
                         if (statusParts.Length < 2 || !int.TryParse(statusParts[0], out int statusId))
                         {
-                            Console.WriteLine("Uso: change-status [ID] [Estado]");
+                            Console.WriteLine("Uso: cambiar-estado [ID] [Estado]");
                         }
                         else if (!Enum.TryParse(statusParts[1], true, out TaskStatusEnum newStatus))
                         {
@@ -75,7 +75,7 @@ namespace QueHaceres_Cli
                         }
                         break;
 
-                    case "list":
+                    case "listar":
                         if (string.IsNullOrEmpty(arguments))
                         {
                             TaskManager.ListTasks();
@@ -90,26 +90,26 @@ namespace QueHaceres_Cli
                         }
                         break;
 
-                    case "help":
+                    case "ayuda":
                         Console.WriteLine("\nComandos disponibles:");
-                        Console.WriteLine("  add \"descripcion\"          - Agrega una nueva tarea");
-                        Console.WriteLine("  update [ID] \"descripcion\"  - Actualiza la descripción de una tarea");
-                        Console.WriteLine("  delete [ID]                 - Elimina una tarea por ID");
-                        Console.WriteLine("  change-status [ID] [Estado] - Cambia el estado de una tarea (Pendiente, EnProgreso, Completada, Cancelada)");
-                        Console.WriteLine("  list [Estado]               - Lista todas las tareas o filtra por estado");
-                        Console.WriteLine("  exit                        - Salir del programa");
+                        Console.WriteLine("  agregar \"descripcion\"              - Agrega una nueva tarea");
+                        Console.WriteLine("  actualizar [ID] \"descripcion\"      - Actualiza la descripción de una tarea");
+                        Console.WriteLine("  eliminar [ID]                      - Elimina una tarea por ID");
+                        Console.WriteLine("  cambiar-estado [ID] [Estado]       - Cambia el estado de una tarea (Pendiente, EnProgreso, Completada, Cancelada)");
+                        Console.WriteLine("  listar [Estado]                    - Lista todas las tareas o filtra por estado");
+                        Console.WriteLine("  salir                              - Salir del programa");
                         break;
 
-                    case "exit":
+                    case "salir":
                         Console.WriteLine("¡Hasta luego!");
                         break;
 
                     default:
-                        Console.WriteLine("Comando desconocido. Escribe 'help' para ver los comandos disponibles.");
+                        Console.WriteLine("Comando desconocido. Escribe 'ayuda' para ver los comandos disponibles.");
                         break;
                 }
 
-            } while (input.ToLower() != "exit");
+            } while (input.ToLower() != "salir");
         }
     }
 }
